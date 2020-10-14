5e57fedab9e31f4231ba1dc3d498574328dc2482
rafiiri <72194087+rafiiri@users.noreply.github.com>
2020-10-14 20:29:28 +0700

    Add files via upload

---
+++ Probabilitas.R
@@ -0,0 +1,11 @@
+###Binomial Random Variable###
+
+
+###
+### For example, You roll a fair die 75 times
+### X is the number of times you get a six/6
+###and the probability of success is 1/6 in each of the 75 trials.
+### What is the probability
+### that we get X = 6 success out of 75 trials?
+
+dbinom(x = 6, size = 75, prob = 1/6)
